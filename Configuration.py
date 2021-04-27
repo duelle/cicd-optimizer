13a23668d5395115cbcc000b3e33c79b7a0b8f35
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2021-04-27 12:04:43 +0200

    Added configration file template.

---
+++ Configuration.py
@@ -0,0 +1,8 @@
+BASE_URL = 'https://api.travis-ci.org'
+HEADERS = {
+    'Travis-API-Version': '3',
+    'User-Agent': 'API Explorer',
+    'Authorization': 'token <auth-token>'
+}
+DATETIME_ISO_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
+DATETIME_ISO_FORMAT_MS = "%Y-%m-%dT%H:%M:%S.%fz"
